3b0c3677c3e0044058a544c063c14927eddb3ce4
karishma <kkunwar211@gmail.com>
2020-07-24 13:17:21 +0545

    public and private vapid keys

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ module.exports = {
     ],
 
     'mongoUrl': 'mongodb://localhost:27017/inventory',
-    'mongoTestUrl':'mongodb://localhost:27017/inventoryTest'
+    'mongoTestUrl':'mongodb://localhost:27017/inventoryTest',
+    'publicKey':'BPJWa9-YHTDjQg-yEED07x6LpapExYxKe1yFERmv0bY0tuudF-SYAn918mO3SGC30SFe0p75olCH5jocgKtjW9c',
+    'privateKey':'d_5d_jCIRMezs_Y4MI0tsUNqjaYZoBTKIVRsSPugIp4'
 }
